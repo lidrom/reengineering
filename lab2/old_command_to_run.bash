0a4d9d475497541c5818d55cba5fd98e3009cf94
leeeek <lidvinskiy@gmail.com>
2024-09-21 13:14:53 +0300

    lab2

---
+++ lab2/old_command_to_run.bash
@@ -0,0 +1,1 @@
+java -XX:StartFlightRecording=filename=recordingOld.jfr,dumponexit=true,settings=profile MainOld && jfr print --events jdk.GCHeapSummary recordingOld.jfr
---
+++ lab2/refactored_command_to_run.bash
@@ -0,0 +1,1 @@
+java -XX:StartFlightRecording=filename=recordingRefactored.jfr,dumponexit=true,settings=profile MainRefactored && jfr print --events jdk.GCHeapSummary recordingRefactored.jfr
